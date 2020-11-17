61d8e239308f123f181401056e3770238be49e70
Shashank Baghel <baghel.shashank7@gmail.com>
2020-11-17 02:43:51 -0800

    First Push

---
+++ src/main/java/com/example/shashank/ShashankApplication.java
@@ -0,0 +1,13 @@
+package com.example.shashank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShashankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShashankApplication.class, args);
+	}
+
+}
